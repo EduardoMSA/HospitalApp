18d4869ffb3634b9982840a3595111147a9adcfd
Eduardo <eduardomndez@ymail.com>
2020-05-22 22:47:13 -0500

    added nevigation

--- app/src/main/java/com/example/hospitalapp/AppointmentActivity.java
+++ app/src/main/java/com/example/hospitalapp/AppointmentActivity.java
@@ -2,7 +2,9 @@ package com.example.hospitalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AppointmentActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class AppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+    }
+
+    public void addNew(View view){
+        startActivity(new Intent(this, AddAppointmentActivity.class));
+    }
+
+    public void viewList(View view){
+        startActivity(new Intent(this, ListAppointmentActivity.class));
     }
 }
